f047c289a2474c4edb0fe2b1c960e2513de5f337
A Pande <akspande1989@gmail.com>
2014-06-29 23:52:28 +0530

    Initial commit

---
+++ build/CMakeFiles/sdl2test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sdl2test.dir/sdl2example.cpp.o"
+  "sdl2test.pdb"
+  "sdl2test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sdl2test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main ()
+
+{
+std::cout  << "Hello World\n"; // just a comment
+return 0;
+
+}
+
